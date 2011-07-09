403b0b4cdb4235836564a667d3e4658b4828d3fa
Steven Gravell <steve.gravell@smarkets.com>
2011-07-09 13:38:53 +0100

    basic app list

---
+++ apps/bigwig/priv/js/tpl.js
@@ -0,0 +1,38 @@
+
+
+tpl = (function() {
+  var title = function(title) {
+    $('#title').text(title);  
+  }
+  title("Loading...");  
+
+  var li = function(p, el) {
+    var id = el.id
+    var li = $('li[data-id='+id+']', p);
+    if(li.length==0) {
+        li = $('li._tpl',p).clone(false, false);
+        li.removeClass('_tpl');
+        p.append(li);
+    }
+    li.text(id);
+  } 
+
+  var sock = new WebSocket("ws://10.100.1.188:8080/wh");
+  sock.onopen = function() {
+
+  }
+  sock.onmessage = function(msg) {
+    var json = $.parseJSON(msg.data);
+    console.log(json);
+    title(json.title);
+    var apps = $('#apps');
+    for(var i=0; i<json.apps.length; i++) {
+      li(apps, json.apps[i]);
+    }
+  }
+  sock.onclose = function() {
+
+  }
+  return {
+  }
+})();
