4735083742a9546e3cd4067160e193a96c0285a2
Steven Gravell <steve.gravell@smarkets.com>
2011-07-09 13:38:53 +0100

    basic app list

--- apps/bigwig/src/bigwig_http_wh.erl
+++ apps/bigwig/src/bigwig_http_wh.erl
@@ -3,25 +3,45 @@
 %%
 -module(bigwig_http_wh).
 -behaviour(cowboy_http_handler).
+-behaviour(cowboy_http_websocket_handler).
 -export([init/3, handle/2, terminate/2]).
+-export([websocket_init/3, websocket_handle/3, websocket_terminate/3]).
 
 -compile(export_all).
 
-init({tcp, http}, Req, _Opts) ->
-  {ok, Req, undefined_state}.
+init({tcp, http}, _Req, _Opts) ->
+  {upgrade, protocol, cowboy_http_websocket}.
 
-handle(Req, State) ->
-  Headers = [{<<"Content-Type">>, <<"text/html">>}],
-  % todo: read file into state on init
-  {ok, Body} = tpl("index"),
-  {ok, Req2} = cowboy_http_req:reply(200, Headers, Body, Req),
-  {ok, Req2, State}.
+handle(_Req, _State) ->
+  error(websockets_only).
 
 terminate(_Req, _State) ->
+  error(websockets_only).
+
+websocket_init(_TransportName, Req, _Opts) ->
+  send({struct, [
+        {title, <<"So Peter... What's Happening?">>},
+        apps()
+      ]}),
+  {ok, Req, undefined_state}.
+
+websocket_handle({timeout, _Ref, Msg}, Req, State) ->
+  {reply, Msg, Req, State};
+
+websocket_handle({websocket, Msg}, Req, State) ->
+  {reply, <<"Hello ", Msg/binary>>, Req, State}.
+
+websocket_terminate(_Reason, _Req, _State) ->
   ok.
 
-tpl(Name) ->
-  file:read_file(tpl_path(Name)).
+send(T) when is_tuple(T) ->
+  send(iolist_to_binary(mochijson2:encode(T)));
 
-tpl_path(Name) ->
-  filename:join([code:priv_dir(bigwig), "html", Name ++ ".html"]).
+send(Bin) ->
+  erlang:start_timer(0, self(), Bin).
+
+apps() ->
+  {apps, [
+      {struct, [{id, App}]}
+      || {App, _Desc, _Vsn} <- application:which_applications()
+    ]}.
