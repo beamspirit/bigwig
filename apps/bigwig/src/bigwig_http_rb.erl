38ab31b0eb0d37b015462b3c3d7e701184c62e69
Hunter Morris <hunter.morris@smarkets.com>
2011-07-09 13:36:25 +0100

    Return rb2 reports as JSON

--- apps/bigwig/src/bigwig_http_rb.erl
+++ apps/bigwig/src/bigwig_http_rb.erl
@@ -25,27 +25,8 @@ handle_path([<<"rb">>, <<"reports">>], Req, State) ->
 handle_path([<<"rb">>, <<"reports">>, IdBin], Req, State) ->
     Id = list_to_integer(binary_to_list(IdBin)),
     Rep = rb2:load_number(Id),
-    %% TODO convert this to JSON:
-    %%  {ok,"2011-07-09 11:42:37",
-    %%      [{report_level,info_report},
-    %%       {group_leader,<0.36.0>},
-    %%       {date,{{2011,7,9},{11,42,37}}},
-    %%       {pid,<0.39.0>},
-    %%       {report_type,progress},
-    %%       {data,[{supervisor,{local,sasl_sup}},
-    %%              {started,[{pid,<0.170.0>},
-    %%                        {name,rb2_server},
-    %%                        {mfargs,{rb2,start_link,[[]]}},
-    %%                        {restart_type,temporary},
-    %%                        {shutdown,brutal_kill},
-    %%                        {child_type,worker}]}]}]}
-    %% 
-    
-    %% TODO Report is the term that needs JSONifying
-    %% {ok, Date, Report, ReportStr} = Report,
-    Body = io_lib:format("~p",[Rep]),
     Headers = [{<<"Content-Type">>, <<"application/json">>}],
-    {ok, Req2} = cowboy_http_req:reply(200, Headers, Body, Req),
+    {ok, Req2} = cowboy_http_req:reply(200, Headers, report_to_json(Rep), Req),
     {ok, Req2, State};
 
 handle_path(Path, Req, State) ->
@@ -55,6 +36,11 @@ handle_path(Path, Req, State) ->
 
 terminate(_Req, _State) ->
     ok.
+
+
+report_to_json({_, {ok, Date0, Report, ReportStr}}) ->
+    Date = list_to_binary(Date0),
+    jsx:term_to_json([{date, Date}, {report, Report}, {report_str, ReportStr}]).
 
 
 list_reports() ->
