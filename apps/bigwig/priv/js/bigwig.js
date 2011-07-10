3c39c396426c499da25083bae2499b147415487e
Steven Gravell <steve.gravell@smarkets.com>
2011-07-10 19:31:39 +0100

    add bigwig.js

---
+++ apps/bigwig/priv/js/bigwig.js
@@ -0,0 +1,30 @@
+
+BigWig = (function() {
+  function activate(tool) {
+    $('#main .tool').removeClass('active');
+    $('#'+tool).addClass('active');
+    $('#tools .tool').removeClass('current');
+    $('#tools *[href='+tool+']').addClass('current');
+    if(tool == "appmon") {
+      APPMON.start();
+    }
+  }
+  (function() {
+    $('#tools .tool').click(function() {
+      activate($(this).attr('href'));
+      return false;
+    });
+    // load in applications
+    $('#applications').bind('onupdate', function(e, data) {
+      TPL.update('running', data.running);
+      //TPL.update('loaded', data.loaded);
+    });
+    TPL.fetch("/vm")
+    activate('etop');
+  })();
+
+  return {
+
+  }
+})();
+
