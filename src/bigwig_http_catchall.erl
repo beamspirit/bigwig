d33898cf05e72606642deaf6602ed004bcc56075
Jack Tang <jack@taodinet.com>
2013-11-07 01:10:23 +0800

    clean the code according to the latest cowboy api

--- src/bigwig_http_catchall.erl
+++ src/bigwig_http_catchall.erl
@@ -1,13 +1,14 @@
 -module(bigwig_http_catchall).
 -behaviour(cowboy_http_handler).
+
 -export([init/3, handle/2, terminate/3]).
 
 init({tcp, http}, Req, _Opts) ->
-    {ok, Req, undefined_state}.
+    {ok, Req, no_state}.
 
 handle(Req, State) ->
     Body = <<"<h1>404</h1>">>,
-    {ok, Req2} = cowboy_http_req:reply(404, [], Body, Req),
+    {ok, Req2} = cowboy_req:reply(404, [], Body, Req),
     {ok, Req2, State}.
 
 terminate(_Reason, _Req, _State) ->
