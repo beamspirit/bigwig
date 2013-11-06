5ac0b6c04e5ea7bdef4b7ce0152917717b189fba
Jack Tang <jack@taodinet.com>
2013-11-07 01:02:00 +0800

    adopt lager logging and let the index page work

--- src/bigwig_app.erl
+++ src/bigwig_app.erl
@@ -10,6 +10,26 @@
 %% ===================================================================
 
 start(_StartType, _StartArgs) ->
+    
+    Dispatch = cowboy_router:compile([
+        %% {URIHost, list({URIPath, Handler, Opts})}
+        {'_', [
+               {"/",              cowboy_static,        {priv_file, bigwig, "html/index.html"}},
+               {"/static/[...]",  cowboy_static,        {priv_dir,  bigwig, "",
+                                                         [{mimetypes, cow_mimetypes, all}]}},
+               {"/module",        bigwig_http_module, []},
+               {'_',              bigwig_http_catchall, []}
+              ]}
+    ]),
+    
+    Port = 40829,
+    %% Name, NbAcceptors, TransOpts, ProtoOpts
+    cowboy:start_http(my_http_listener, 100,
+                      [{port, Port}],
+                      [{env, [{dispatch, Dispatch}]}]
+    ),
+
+    lager:info("Bigwig listening on port ~B", [Port]),
     bigwig_sup:start_link().
 
 stop(_State) ->
--- start-dev.sh
+++ start-dev.sh
@@ -2,4 +2,4 @@
 cd `dirname $0`
 make
 mkdir -p log/sasl
-erl -sname bigwig -pa $PWD/ebin $PWD/deps/*/ebin -boot start_sasl -config sys.config -s reloader -s bigwig
+erl -name bigwig@localhost -pa $PWD/ebin $PWD/deps/*/ebin -boot start_sasl -config sys.config -s reloader -s bigwig
