eeb84d68de9c1c64615a47b75f43385305cf8c3d
Jack Tang <jack@taodinet.com>
2013-11-07 23:30:13 +0800

    let etop2 work

--- include/observer_backend.hrl
+++ include/observer_backend.hrl
@@ -16,27 +16,27 @@
 %%     $Id$
 %%
 
--record(etop_info, 
-	{now = {0, 0, 0},
-	 n_procs = 0,
-	 wall_clock = {0, 0},
-	 runtime = {0, 0},
-	 run_queue = 0,
-	 alloc_areas = [],
-	 memi = [{total, 0},
-		 {processes, 0}, 
-		 {ets, 0},
-		 {atom, 0},
-		 {code, 0},
-		 {binary, 0}],
-	 procinfo = []
-	}).
+-record(etop_info, {
+          now = {0, 0, 0},
+          n_procs = 0,
+          wall_clock = {0, 0},
+          runtime = {0, 0},
+          run_queue = 0,
+          alloc_areas = [],
+          memi = [{total, 0},
+                  {processes, 0}, 
+                  {ets, 0},
+                  {atom, 0},
+                  {code, 0},
+                  {binary, 0}],
+          procinfo = []
+         }).
 
--record(etop_proc_info,
-	{pid,
-	 mem=0,
-	 reds=0,
-	 name,
-	 runtime=0,
-	 cf,
-	 mq=0}).
+-record(etop_proc_info, {
+          pid,
+          mem=0,
+          reds=0,
+          name,
+          runtime=0,
+          cf,
+          mq=0}).
