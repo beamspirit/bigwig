c981589fe4ec783e8cdd911b26ccec8d599cbf85
Zhaoren Yu <zhaoren.yu@taodinet.com>
2013-11-22 10:59:17 +0800

    #5107 websocket

---
+++ priv/js/lager.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+    connect("/lager/stream");
+}
+function connect(to)
+{
+       var host = document.location.host;
+       websocket = new WebSocket("ws://"+host+to);
+       websocket.onopen = function(evt) { onOpen(evt) }; 
+       websocket.onclose = function(evt) { onClose(evt) }; 
+       websocket.onmessage = function(evt) { onMessage(evt) }; 
+};  
+      
+function onOpen(evt) { 
+};  
+function sendTxt(txt) {
+    websocket.send(txt);
+};
+function onClose(evt) { 
+};  
+
+function onMessage(evt) { 
+    $('#trace_log').prepend('<p>' + txt + '</p>');
+};  
+
+$("body").delegate("a.tracer", "click", function(e){
+     var x1 = $(this).text();
+     sendTxt(x1);
+     e.preventDefault();
+});
---
+++ src/bigwig_http_lager_stream.erl
@@ -0,0 +1,38 @@
+%% Websocket connection that sends log of trace
+%%
+-module(bigwig_http_lager_stream).
+-behaviour(cowboy_websocket_handler).
+
+-export([init/3, handle/2, terminate/3]).
+-export([websocket_init/3, websocket_handle/3,
+				 websocket_info/3, websocket_terminate/3]).
+
+
+init({tcp, http}, _Req, _Opts) ->
+    {upgrade, protocol, cowboy_websocket}.
+
+handle(_Req, _State) ->
+    exit(websockets_only).
+
+terminate(_Reason, _Req, _State) ->
+    exit(websockets_only).
+
+websocket_init(_TransportName, Req, _Opts) ->
+    bigwig_pubsubhub:register_client(self()),
+    {ok, Req, undefined_state}.
+
+%% TODO handle stuff like {bigwig, {appmon, ... }} and send that too
+websocket_handle({text, _Msg}, Req, State) ->
+    amqp_subscriber:start_link(<<"trace.*">>),
+    {ok, Req, State}.
+websocket_info({bigwig, {bigwig_trace, Stats}}, Req, State) ->
+     Reply = jsx:term_to_json([{lager, Stats}]),
+     {reply, {text, Reply}, Req, State};
+websocket_info({bigwig, _}, Req, State) ->
+     {ok, Req, State};
+websocket_info(Info, Req, State) ->
+     io:format("Unhandled msg to ~p ~p\n", [?MODULE, Info]),
+     {ok, Req, State}.
+
+websocket_terminate(_Reason, _Req, _State) -> ok.
+
