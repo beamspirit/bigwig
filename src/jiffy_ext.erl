399f7934f1eb489083c5f8922366636caa5301b4
Zhaoren Yu <zhaoren.yu@taodinet.com>
2013-12-03 18:35:18 +0800

    #5151 add jst_ext encode

--- src/jiffy_ext.erl
+++ src/jiffy_ext.erl
@@ -9,11 +9,23 @@
 -module(jiffy_ext).
 
 %% API
--export([]).
+-export([encode/1]).
 
 %%%===================================================================
 %%% API
 %%%===================================================================
+encode(Binary) when is_binary(Binary) ->
+    jiffy_ext:encode(Binary);
+encode(Atom) when is_atom(Atom) ->
+    jiffy:encode(Atom);
+encode(Pid) when is_pid(Pid) ->
+    jiffy:encode(list_to_binary(erlang:pid_to_list(Pid)));
+encode(Port) when is_port(Port) ->
+    jiffy:encode(list_to_binary(erlang:port_to_list(Port)));
+encode(Ref) when is_reference(Ref) ->
+    jiffy:encode(list_to_binary(erlang:ref_to_list(Ref)));
+encode({{Y,M,D},{Hrs,Mins,Secs}}) ->
+    jiffy_ext:encode([Y,M,D,Hrs,Mins,Secs]).
 
 %%--------------------------------------------------------------------
 %% @doc
