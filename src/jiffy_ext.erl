1c607d19573dd0af6b8ff7ff4b1eab645d8613fa
Zhaoren Yu <zhaoren.yu@taodinet.com>
2013-12-05 10:27:00 +0800

    #5151

--- src/jiffy_ext.erl
+++ src/jiffy_ext.erl
@@ -9,30 +9,44 @@
 -module(jiffy_ext).
 
 %% API
--export([encode/1]).
+-export([encode/1,
+        to_jiffy/1]).
+
 
 %%%===================================================================
 %%% API
 %%%===================================================================
-encode(Binary) when is_binary(Binary) ->
-    jiffy_ext:encode(Binary);
-encode(Atom) when is_atom(Atom) ->
-    jiffy:encode(Atom);
-encode(Pid) when is_pid(Pid) ->
-    jiffy:encode(list_to_binary(erlang:pid_to_list(Pid)));
-encode(Port) when is_port(Port) ->
-    jiffy:encode(list_to_binary(erlang:port_to_list(Port)));
-encode(Ref) when is_reference(Ref) ->
-    jiffy:encode(list_to_binary(erlang:ref_to_list(Ref)));
-encode({{Y,M,D},{Hrs,Mins,Secs}}) ->
-    jiffy_ext:encode([Y,M,D,Hrs,Mins,Secs]).
-
+encode(Data) ->
+    Jiffy=to_jiffy(Data),
+    jiffy:encode(Jiffy).
 %%--------------------------------------------------------------------
 %% @doc
 %% @spec
 %% @end
 %%--------------------------------------------------------------------
-
 %%%===================================================================
 %%% Internal functions
 %%%===================================================================
+to_jiffy(Tuple) when is_tuple(Tuple) ->
+    to_jiffy_list(tuple_to_list(Tuple),[]);
+to_jiffy(Port) when is_port(Port) ->
+    list_to_binary(erlang:port_to_list(Port));
+to_jiffy(Ref) when is_reference(Ref) ->
+    list_to_binary(erlang:ref_to_list(Ref));            
+to_jiffy(Pid) when is_pid(Pid) ->
+    list_to_binary(erlang:pid_to_list(Pid));
+to_jiffy(Values) when is_list(Values) ->
+    to_jiffy_list(Values, []);
+to_jiffy(Val) ->
+    Val.
+
+
+to_jiffy_list([], Acc) ->
+    lists:reverse(Acc);
+to_jiffy_list([Val | Rest], Acc0) ->
+    Acc = [to_jiffy(Val) | Acc0],
+    to_jiffy_list(Rest, Acc).
+
+
+
+
