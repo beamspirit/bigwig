6d15ece34924dba8ceeb06669a8d8b1efdf79a0e
Zhaoren <zhaoren.yu@taodinet.com>
2014-01-09 14:16:10 +0800

    #5216

--- priv/js/md_stat.js
+++ priv/js/md_stat.js
@@ -17,21 +17,60 @@ $(document).ready(function(){
         onAnimationPaused:false, // call back for animation upon pausing
         onAnimationResumed:false // call back for animation upon resuming from pause
     });
+    $('#bandwidth').highcharts({
+        chart: {
+            type: 'area'
+        },
+        title: {
+            text: 'BandWidth Consumption Of Every MD Node'
+        },
+        xAxis: {
+            categories: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23]
+        },
+        yAxis: {
+            title: {
+                text: 'BandWidth Consumption'
+            },
+            labels: {
+                formatter: function() {
+                    return this.value +'k';
+                }
+            }
+        },
+        tooltip: {
+            pointFormat: '{series.name} produced <b>{point.y:,.0f}</b><br/>warheads in {point.x}'
+        },
+        plotOptions: {
+            area: {
+                pointStart: 0,
+                marker: {
+                    enabled: false,
+                    symbol: 'circle',
+                    radius: 2,
+                    states: {
+                        hover: {
+                            enabled: true
+                        }
+                    }
+                }
+            }
+        },
+        series: [{
+            name: 'market_dispatch1@dispatch.lk.com',
+            data: [6 , 11, 32, 11, 23, 36, 64,
+                10, 14, 20, 30, 46, 64, 98, 15, 20, 24,
+                27, 29, 31, 31, 32, 31, 29]
+        }, {
+            name: 'market_dispatch2@dispatch.lk.com',
+            data: [5, 25, 50, 12, 15, 20, 42, 66, 86, 10, 16, 24, 33,
+            42, 52, 61, 70, 39, 93, 38, 43, 13, 78,
+            15]
+        }]
+    });
     chart = new Highcharts.Chart({
             chart: {
                 renderTo: 'charts',
-                type: 'column',
-                events :{
-                    load: function() {      
-                  // set up the updating of the chart each second  
-                    var series = this.series[2];  
-                    setInterval(function() {  
-                        var data = [];  
-                        data.push([2, Math.random()]);  
-                        series.setData(data);  
-                    }, 1000);  
-                }}
-            
+                type: 'column'
             },
             title: {
                 text: 'MD Statistic'
@@ -80,14 +119,11 @@ $(document).ready(function(){
                 }
             },
             series: [{
-                name: 'market_dispatch@dispatch.lk.com',
-                data: [5, 3, 4]
-            }, {
                 name: 'market_dispatch1@dispatch.lk.com',
-                data: [2, 2, 3]
+                data: [5, 3, 4, 6, 7, 8, 10, 15, 6, 11, 13, 16, 21, 14, 12, 7, 50, 24, 23, 20, 17, 22, 17, 15]
             }, {
                 name: 'market_dispatch2@dispatch.lk.com',
-                data: [3, 4, 4]
+                data: [3, 4, 4, 5, 3, 4, 6, 7, 8, 10, 15, 6, 11, 13, 16, 21, 14, 12, 7, 50, 24, 23, 20, 17]
             }]
         });
     connect("/md/stream");
@@ -115,6 +151,33 @@ function onMessage(evt) {
 	  {
     	$('#md_statistic').append('<p>' + node + '</p>');
     	$("#counter").flipCounter("setNumber", msg[node]);
+        var series  = chart.series[0];
+        var data = [];
+        data.push([0, 4]);
+        data.push([1, 7]);
+        data.push([2, 8]);
+        data.push([3, 9]);
+        data.push([4, 10]);
+        data.push([5, 13]);
+        data.push([6, 14]);
+        data.push([7, 9]);
+        data.push([8, 8]);
+        data.push([9, 16]);
+        data.push([10, 22]);
+        data.push([11, 24]);
+        data.push([12, 18]);  
+        data.push([13, 29]);
+        data.push([14, 16]);
+        data.push([15, 22]);
+        data.push([16, 11]);
+        data.push([17, 44]);
+        data.push([18, 33]);
+        data.push([19, 19]);
+        data.push([20, 24]);
+        data.push([21, 23]);
+        data.push([22, 13]);
+        data.push([23, msg[node]]);
+        series.setData(data);
 	  }
     
 
